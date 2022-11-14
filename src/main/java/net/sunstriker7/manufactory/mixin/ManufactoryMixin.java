e1dc374a3655248619ffa92319de964440dcc2bb
sunstriker7 <sunstriker7@gmail.com>
2022-11-13 22:09:25 -0700

    Initial commit

---
+++ src/main/java/net/sunstriker7/manufactory/mixin/ManufactoryMixin.java
@@ -0,0 +1,16 @@
+package net.sunstriker7.manufactory.mixin;
+
+import net.sunstriker7.manufactory.Manufactory;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ManufactoryMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		Manufactory.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
